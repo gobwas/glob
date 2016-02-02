61a66d485f0d192ec0b17befc1cd20025aff864f
gobwas <gobwas@gmail.com>
2016-02-02 22:20:26 +0300

    tune

--- glob_test.go
+++ glob_test.go
@@ -168,6 +168,15 @@ func BenchmarkAllGlobMatch(b *testing.B) {
 		_ = m.Match(fixture_all_match)
 	}
 }
+func BenchmarkAllGlobMatchParallel(b *testing.B) {
+	m, _ := Compile(pattern_all)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_ = m.Match(fixture_all_match)
+		}
+	})
+}
 func BenchmarkAllRegexpMatch(b *testing.B) {
 	m := regexp.MustCompile(regexp_all)
 	f := []byte(fixture_all_match)
--- match/match.go
+++ match/match.go
@@ -42,8 +42,15 @@ func toPowerOfTwo(v int) int {
 	return v
 }
 
+const (
+	minSegment         = 32
+	minSegmentMinusOne = 31
+	maxSegment         = 1024
+	maxSegmentMinusOne = 1023
+)
+
 func init() {
-	for i := 1024; i >= 1; i >>= 1 {
+	for i := maxSegment; i >= minSegment; i >>= 1 {
 		func(i int) {
 			segmentsPools[i-1] = sync.Pool{
 				New: func() interface{} {
@@ -54,41 +61,29 @@ func init() {
 	}
 }
 
-var segmentsPool = sync.Pool{
-	New: func() interface{} {
-		return make([]int, 0, 64)
-	},
-}
-
 func getIdx(c int) int {
 	p := toPowerOfTwo(c)
 	switch {
-	case p >= 1024:
-		return 1023
-	case p < 1:
-		return 0
+	case p >= maxSegment:
+		return maxSegmentMinusOne
+	case p <= minSegment:
+		return minSegmentMinusOne
 	default:
 		return p - 1
 	}
 }
 
 func acquireSegments(c int) []int {
+	//	fmt.Println("GET", getIdx(c))
 	return segmentsPools[getIdx(c)].Get().([]int)[:0]
 }
 
 func releaseSegments(s []int) {
+	//	fmt.Println("PUT", getIdx(cap(s)))
 	segmentsPools[getIdx(cap(s))].Put(s)
 }
 
-func appendIfNotAsPrevious(target []int, val int) []int {
-	l := len(target)
-	if l != 0 && target[l-1] == val {
-		return target
-	}
-
-	return append(target, val)
-}
-
+// appendMerge merges and sorts given already SORTED and UNIQUE segments.
 func appendMerge(target, sub []int) []int {
 	lt, ls := len(target), len(sub)
 	out := acquireSegments(lt + ls)
@@ -129,64 +124,6 @@ func appendMerge(target, sub []int) []int {
 	releaseSegments(out)
 
 	return target
-}
-
-// mergeSegments merges and sorts given already SORTED and UNIQUE segments.
-func mergeSegments(list [][]int, out []int) []int {
-	var current []int
-	switch len(list) {
-	case 0:
-		return out
-	case 1:
-		return list[0]
-	default:
-		current = acquireSegments(len(list[0]))
-		current = append(current, list[0]...)
-		//		releaseSegments(list[0])
-	}
-
-	for _, s := range list[1:] {
-		next := acquireSegments(len(current) + len(s))
-		for x, y := 0, 0; x < len(current) || y < len(s); {
-			if x >= len(current) {
-				next = append(next, s[y:]...)
-				break
-			}
-
-			if y >= len(s) {
-				next = append(next, current[x:]...)
-				break
-			}
-
-			xValue := current[x]
-			yValue := s[y]
-
-			switch {
-
-			case xValue == yValue:
-				x++
-				y++
-				next = appendIfNotAsPrevious(next, xValue)
-
-			case xValue < yValue:
-				next = appendIfNotAsPrevious(next, xValue)
-				x++
-
-			case yValue < xValue:
-				next = appendIfNotAsPrevious(next, yValue)
-				y++
-
-			}
-		}
-
-		releaseSegments(current)
-		current = next
-	}
-
-	out = append(out, current...)
-	releaseSegments(current)
-
-	return out
 }
 
 func reverseSegments(input []int) {
