80c58b01f2b00664c92a1103bd9d9e328810d758
Sergey Kamardin <gobwas@gmail.com>
2026-08-17 07:21:01 +0100

    bench.sh: fix the assignment and the benchstat args, ignore *.bench

--- bench.sh
+++ bench.sh
@@ -1,32 +1,41 @@
-#! /bin/bash
+#!/bin/bash
+#
+# Compares the benchmarks of the current branch against a git revision:
+#
+#	./bench.sh v0.2.3            # all the benchmarks
+#	./bench.sh master 'Match'    # the ones matching a -bench regexp
+#
+# The results are written to *.bench files in the current directory and
+# compared with benchstat (go install golang.org/x/perf/cmd/benchstat@latest).
 
-set -u
+set -eu
 
 prev=$1
-what=$2
-curr=`git rev-parse --abbrev-ref HEAD`
-rnd=$(head -c4 </dev/urandom|xxd -p)
+what=${2:-.}
+curr=$(git rev-parse --abbrev-ref HEAD)
+rnd=$(head -c4 </dev/urandom | xxd -p)
 
-bench() {
-	local exp=".*"
-    if [[ ! -z $2 ]]; then
-    	$exp = $2
-    fi
-    filename=$(echo "$rnd-$1.bench" | tr "/" "_")
-    if [[ -e "${filename}" ]]; then
-        echo "Already exists ${filename}"
-    else
-        local backup=`git rev-parse --abbrev-ref HEAD`
-        git checkout "$1"
-        echo -n "Creating ${filename}... "
-        go test ./... -run=none -benchmem -bench="$exp" > "${filename}"
-        echo "OK"
-        git checkout ${backup}
-        sleep 5
-    fi
+file() {
+	echo "$rnd-$1.bench" | tr "/" "_"
 }
 
-bench ${prev} ${what}
-bench ${curr} ${what}
+bench() {
+	local rev=$1
+	local out
+	out=$(file "$rev")
+	if [[ -e "$out" ]]; then
+		echo "Already exists $out"
+		return
+	fi
+	git checkout -q "$rev"
+	echo -n "Creating $out... "
+	go test ./... -run=none -benchmem -bench="$what" >"$out"
+	echo "OK"
+	git checkout -q "$curr"
+	sleep 5
+}
 
-benchstat "$rnd-${to}.bench" "$rnd-${current}.bench"
+bench "$prev"
+bench "$curr"
+
+benchstat "$(file "$prev")" "$(file "$curr")"
